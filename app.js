526775d0c42e97794bf94c19c1f79ade8e2b1866
Spencer Rose <Spencer.Rose@gov.bc.ca>
2023-03-20 15:56:26 -0700

    Reorganize Node web server.

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ dotenv.config()
 const nodeENV = process.env.NODE_ENV;
 const baseURL = process.env.LSA_APPS_BASE_URL;
 const apiURL = process.env.LSA_APPS_API_URL;
-const appPort = process.env.LSA_APPS_REGISTRATION_PORT || 3000;
-const appURL = process.env.LSA_APPS_REGISTRATION_URL;
+const appPort = process.env.LSA_APPS_ADMIN_PORT || 3000;
+const appURL = process.env.LSA_APPS_ADMIN_URL;
 
 // configure CORS allowed hostnames
 const allowedOrigins = [baseURL, appURL, apiURL];
