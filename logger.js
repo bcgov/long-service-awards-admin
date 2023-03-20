61130f6d69f9aec327aab0ccee625620b7d321db
Spencer Rose <Spencer.Rose@gov.bc.ca>
2023-03-20 15:33:09 -0700

    Reorganize Node web server.

---
+++ logger.js
@@ -0,0 +1,46 @@
+/*!
+ * API request loggers
+ * File: logger.js
+ * Copyright(c) 2022 BC Gov
+ * MIT Licensed
+ */
+
+// init env variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+import winston from 'winston';
+
+export default null;
+
+export const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+    exceptionHandlers: [new winston.transports.File({ filename: "exceptions.log" })],
+    rejectionHandlers: [new winston.transports.File({ filename: "rejections.log" })],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple(),
+    }));
+}
+
+export const requestLogger = function (req, res, next) {
+    const d = new Date();
+    logger.info(`[${process.env.NODE_ENV}] - ${req.originalUrl} - ${req.method} - ${req.ip}`, d);
+    next();
+}
