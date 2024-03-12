e71ae27fb973d779750c89a1712f627ebff66123
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2024-03-12 08:54:46 -0700

    Add testing banner.

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { useStatus } from "@/providers/status.provider.jsx";
 import MenuBar from "@/components/common/MenuBar.jsx";
 import { BlockUI } from "primereact/blockui";
 import { useLocation } from "react-router-dom";
+import dotenv from "dotenv";
 
 /**
  * Global Router provider.
@@ -21,12 +22,18 @@ import { useLocation } from "react-router-dom";
 export default function App() {
   const { loading } = useStatus();
   const location = useLocation();
+  const nodeENV = process.env.NODE_ENV;
   return (
     // Do not display header on RSVP registration
     <>
       {/* {!location.pathname.includes("/rsvp/") && ( */}
       <header>
         <BlockUI blocked={loading}>
+          {nodeENV === "development" ? (
+            <div className="w-screen bg-orange-500 fixed top-0 z-9999 m-0 text-center">
+              Test Environment
+            </div>
+          ) : null}
           <MenuBar />
         </BlockUI>
       </header>
