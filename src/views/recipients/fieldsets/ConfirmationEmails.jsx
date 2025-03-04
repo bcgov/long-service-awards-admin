172d50c9761f0a324996eb66eef0a8f881b9c51b
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2025-03-04 14:50:52 -0800

    LSA-537 Prevent function not found error when sending confirmation emails

--- src/views/recipients/fieldsets/ConfirmationEmails.jsx
+++ src/views/recipients/fieldsets/ConfirmationEmails.jsx
@@ -5,71 +5,80 @@
  * MIT Licensed
  */
 
-import {useFormContext, useWatch} from "react-hook-form";
-import React, {useEffect, useState} from "react";
-import {ConfirmDialog} from "primereact/confirmdialog";
-import {Button} from "primereact/button";
-import {useStatus} from "@/providers/status.provider.jsx";
-import {Panel} from "primereact/panel";
-import {useAPI} from "@/providers/api.provider.jsx";
+import { useFormContext, useWatch } from "react-hook-form";
+import React, { useEffect, useState } from "react";
+import { ConfirmDialog } from "primereact/confirmdialog";
+import { Button } from "primereact/button";
+import { useStatus } from "@/providers/status.provider.jsx";
+import { Panel } from "primereact/panel";
+import { useAPI } from "@/providers/api.provider.jsx";
 
 /**
  * Registration confirmation input component.
  * @returns {JSX.Element},
  */
 
-export default function ConfirmationEmails({visible=false}) {
+export default function ConfirmationEmails({ visible = false }) {
+  // set local states
+  const api = useAPI();
+  const status = useStatus();
+  const [showDialog, setShowDialog] = useState(false);
+  const { getValues } = useFormContext();
+  const isConfirmed = useWatch({ name: "service.confirmed" });
 
-    // set local states
-    const api = useAPI();
-    const status = useStatus();
-    const [showDialog, setShowDialog] = useState(false);
-    const { getValues } = useFormContext();
-    const isConfirmed = useWatch({name: 'service.confirmed'});
+  // show email confirmation on submission
+  useEffect(() => {
+    setShowDialog(visible);
+  }, [visible]);
 
-    // show email confirmation on submission
-    useEffect(() => {
-        setShowDialog(visible)
-    }, [visible])
+  /**
+   * Send confirmation emails to recipient and supervisor
+   */
 
-    /**
-     * Send confirmation emails to recipient and supervisor
-     */
+  const _sendMail = async (e) => {
+    if (e && e.preventDefault) e.preventDefault();
+    status.setMessage("mail");
+    api
+      .sendMail("reg-confirm", getValues())
+      .then(([error]) => {
+        if (error) status.setMessage("mailError");
+        else status.setMessage("mailSuccess");
+      })
+      .catch((error) => {
+        console.error(error);
+        status.setMessage("mailError");
+      });
+  };
 
-    const _sendMail = async (e) => {
-        if (e) e.preventDefault();
-        status.setMessage('mail');
-        api.sendMail('reg-confirm', getValues())
-            .then(([error, ]) => {
-                if (error) status.setMessage('mailError');
-                else status.setMessage('mailSuccess');
-            })
-            .catch(error => {
-                console.error(error)
-                status.setMessage('mailError');
-            });
-    }
-
-    return <>
-        {
-            isConfirmed && visible && <Panel className={'mb-3'} collapsed={false} toggleable={false} header={'Send Confirmation Emails'}>
-                <Button
-                    className={'w-full'}
-                    onClick={_sendMail}
-                    icon={'pi pi-envelope'}
-                    label={'Send Confirmation Emails'}
-                />
-            </Panel>
-        }
-        <ConfirmDialog
-            header="Email Confirmation"
-            visible={showDialog}
-            onHide={() => setShowDialog(false)}
-            onClick={(e)=>{e.stopPropagation()}}
-            message="Send registration confirmation emails to recipient and supervisor?"
-            icon="pi pi-envelope"
-            accept={_sendMail}
-            reject={() => setShowDialog(false)}
-        />
+  return (
+    <>
+      {isConfirmed && visible && (
+        <Panel
+          className={"mb-3"}
+          collapsed={false}
+          toggleable={false}
+          header={"Send Confirmation Emails"}
+        >
+          <Button
+            className={"w-full"}
+            onClick={_sendMail}
+            icon={"pi pi-envelope"}
+            label={"Send Confirmation Emails"}
+          />
+        </Panel>
+      )}
+      <ConfirmDialog
+        header="Email Confirmation"
+        visible={showDialog}
+        onHide={() => setShowDialog(false)}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        message="Send registration confirmation emails to recipient and supervisor?"
+        icon="pi pi-envelope"
+        accept={_sendMail}
+        reject={() => setShowDialog(false)}
+      />
     </>
+  );
 }
